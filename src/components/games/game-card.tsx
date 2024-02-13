a50e9cdf07fcf5f7b1f298c7cff56f60e8fd6cd8
specificlanguage <specificlanguage@gmail.com>
2024-02-13 18:41:48 -0500

    feat(games): Team view rotators
    - On team name click on game page, allows user to rotate between abbreviation view, team name view or full name view

--- src/components/games/game-card.tsx
+++ src/components/games/game-card.tsx
@@ -4,6 +4,7 @@ import { TeamLogo } from "@/components/teams/logos.tsx";
 import { useFetchTeams } from "@/lib/http/teams.ts";
 import { Skeleton } from "@/components/ui/skeleton.tsx";
 import GameInfo from "@/components/games/game-info.tsx";
+import { useState } from "react";
 
 interface GameCardProps {
   game: Game;
@@ -12,32 +13,57 @@ interface GameCardProps {
 export default function GameCard({ game }: GameCardProps) {
   const { data } = useFetchTeams();
 
+  const possibleViews = ["team", "abbr", "full"];
+  const [teamView, setTeamView] = useState<number>(0);
+
   const awayTeam = data?.find((team) => team.id === game.awayTeam_id);
   const homeTeam = data?.find((team) => team.id === game.homeTeam_id);
+
+  function onClick() {
+    setTeamView((teamView + 1) % possibleViews.length);
+  }
+
+  function getDisplay(team: typeof awayTeam | typeof homeTeam) {
+    if (possibleViews[teamView] === "abbr") {
+      return team?.abbr;
+    } else if (possibleViews[teamView] === "full") {
+      return team?.name;
+    } else {
+      return team?.teamName;
+    }
+  }
 
   return (
     <Card data-testid={game.id}>
       <CardContent data-testid="game-card" className="my-2 pb-0 space-y-2">
-        <div className="flex justify-center">
-          {awayTeam ? (
-            <TeamLogo
-              imageOrientation={"left"}
-              label={game.awayName}
-              team={awayTeam}
-              height={32}
-              imageScheme="spot"
-            />
-          ) : (
-            <Skeleton className="h-[32px] w-[32px]" />
-          )}
-          <div className="flex justify-between">
+        <div className="flex flex-row">
+          <div
+            className="flex justify-end basis-[48%]"
+            onClick={() => onClick()}
+          >
+            {awayTeam ? (
+              <TeamLogo
+                imageOrientation={"left"}
+                label={getDisplay(awayTeam)}
+                team={awayTeam}
+                height={32}
+                imageScheme="spot"
+              />
+            ) : (
+              <Skeleton className="h-[32px] w-[32px]" />
+            )}
+          </div>
+          <div className="basis-[4%]">
             <p className="mx-2 h-[32px] text-center leading-[32px]">@</p>
           </div>
-          <div className="flex justify-between">
+          <div
+            className="flex justify-start basis-[48%]"
+            onClick={() => onClick()}
+          >
             {homeTeam ? (
               <TeamLogo
                 imageOrientation={"right"}
-                label={game.homeName}
+                label={getDisplay(homeTeam)}
                 team={homeTeam}
                 height={32}
                 imageScheme="spot"
