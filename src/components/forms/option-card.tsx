764ee967a5a7692f6e47870b8ae97c38f735ddf6
specificlanguage <specificlanguage@gmail.com>
2024-02-19 22:43:01 -0800

    feat(picks): Add fillPct field to option card
    - Adds a fillPct prop to option card for showing other player's picks for the card

--- src/components/forms/option-card.tsx
+++ src/components/forms/option-card.tsx
@@ -5,6 +5,7 @@ import { ReactNode } from "react";
 interface OptionCardProps {
   children: ReactNode;
   value: string;
+  fillPct?: number;
   className?: string;
   disabled?: boolean;
 }
@@ -13,6 +14,7 @@ interface OptionCardProps {
  * OptionCard -- a component to render a card for a radio group option.
  * @param children - the content of the option
  * @param value - the value of the option
+ * @param fillPct - the percentage of the card to fill with a color -- if not present, it won't be filled.
  * @param className - additional classes to add to the card
  * @param disabled - whether the option is disabled
  * @param props - any other props to pass to the radio group item
@@ -21,17 +23,23 @@ interface OptionCardProps {
 export function OptionCard({
   children,
   value,
+  fillPct,
   className,
   disabled,
   ...props
 }: OptionCardProps) {
   return (
-    <div className="my-1">
+    <div className="my-1 relative">
+      {fillPct ? (
+        <div
+          className={`absolute top-0 left-0 p-0.25 rounded-tl-sm h-full bg-neutral-300 dark:bg-neutral-800 w-[${fillPct}%]`}
+        ></div>
+      ) : null}
       <Label
         htmlFor={value}
         className={
           "flex flex-col items-center justify-between \
-                      rounded-md border-2 border-muted bg-popover p-4 leading-8 \
+                      rounded-md border-2 border-muted bg-popover p-2.5 leading-8 \
                       hover:bg-accent hover:text-accent-foreground \
                       peer-data-[state=checked]:border-primary \
                       [&:has([data-state=checked])]:border-primary relative peer " +
@@ -46,6 +54,7 @@ export function OptionCard({
           {...props}
         />
         {children}
+        {fillPct ? <p className="justify-end mt-3 leading-3">96%</p> : null}
       </Label>
     </div>
   );
