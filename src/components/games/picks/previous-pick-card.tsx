6333a3d69edd5dfe862c8beaa1b6c38780263e92
specificlanguage <specificlanguage@gmail.com>
2024-04-07 22:14:35 -0400

    feat: Modify previous pick cards to match others
    - Pick cards now use the flex-row instead of a grid
    - Names now are the team names instead of the cities
    - Dividers included to separate the pick cards

--- src/components/games/picks/previous-pick-card.tsx
+++ src/components/games/picks/previous-pick-card.tsx
@@ -12,6 +12,7 @@ import { SiMlb } from "react-icons/si";
 import { Button } from "@/components/ui/button.tsx";
 import { useAuth } from "@clerk/clerk-react";
 import { usePrefs } from "@/lib/http/users.ts";
+import { Separator } from "@/components/ui/separator.tsx";
 
 interface PreviousPickCardProps {
   game: Game;
@@ -30,51 +31,64 @@ export function PreviousPickCard({ game, pick }: PreviousPickCardProps) {
   return (
     <Card>
       <CardContent className="p-0 m-0 space-y-2">
-        <div className="grid grid-cols-5 space-4 p-1">
-          <div className="col-span-2 flex justify-start space-x-2 p-2">
-            <TeamLogo
-              imageOrientation={"left"}
-              useLabel={"city"}
-              team={awayTeam}
-              height={32}
-              imageScheme="spot"
-            />
-            {prefs?.favoriteTeam_id === awayTeam.id && <FavoriteTeamIcon />}
+        <div className="flex flex-row space-x-4 p-1">
+          <div className="basis-5/12 flex justify-between space-x-2 p-2">
+            {/* Away team */}
+            <div className="flex justify-start space-x-2">
+              <TeamLogo
+                imageOrientation={"left"}
+                label={awayTeam.teamName}
+                team={awayTeam}
+                height={32}
+                imageScheme="spot"
+              />
+              {prefs?.favoriteTeam_id === awayTeam.id && <FavoriteTeamIcon />}
+              <p className="leading-8">
+                {pick !== undefined &&
+                  !pick.pickedHome &&
+                  (game.finished && game.winner === awayTeam.id ? (
+                    <CheckedIcon />
+                  ) : (
+                    <HollowCheckedIcon />
+                  ))}
+              </p>
+            </div>
             <p className="text-xl font-bold leading-8">
               {game.away_score === undefined ? null : game.away_score}
             </p>
-            <p className="leading-8">
-              {pick !== undefined &&
-                !pick.pickedHome &&
-                (game.finished && game.winner === awayTeam.id ? (
-                  <CheckedIcon />
-                ) : (
-                  <HollowCheckedIcon />
-                ))}
-            </p>
           </div>
 
-          <div className="col-span-2 flex justify-start space-x-2 p-2">
-            <TeamLogo
-              imageOrientation={"left"}
-              useLabel={"city"}
-              team={homeTeam}
-              height={32}
-              imageScheme="spot"
-            />
-            {prefs?.favoriteTeam_id === homeTeam.id && <FavoriteTeamIcon />}
+          <div className="basis-1/16 py-2">
+            <Separator orientation="vertical" />
+          </div>
+
+          {/* Home team */}
+          <div className="basis-5/12 flex justify-between space-x-2 p-2">
+            <div className="flex justify-start space-x-2">
+              <TeamLogo
+                imageOrientation={"left"}
+                label={homeTeam.teamName}
+                team={homeTeam}
+                height={32}
+                imageScheme="spot"
+              />
+              {prefs?.favoriteTeam_id === homeTeam.id && <FavoriteTeamIcon />}
+              <p className="leading-8">
+                {pick !== undefined &&
+                  pick?.pickedHome &&
+                  (game.finished && game.winner === homeTeam.id ? (
+                    <CheckedIcon />
+                  ) : (
+                    <HollowCheckedIcon />
+                  ))}
+              </p>
+            </div>
             <p className="text-xl font-bold leading-8">
               {game.home_score === undefined ? null : game.home_score}
             </p>
-            <p className="leading-8">
-              {pick !== undefined &&
-                pick?.pickedHome &&
-                (game.finished && game.winner === homeTeam.id ? (
-                  <CheckedIcon />
-                ) : (
-                  <HollowCheckedIcon />
-                ))}
-            </p>
+          </div>
+          <div className="basis-1/16 py-2">
+            <Separator orientation="vertical" />
           </div>
           <div className="flex justify-start m-2">
             <a
@@ -82,7 +96,7 @@ export function PreviousPickCard({ game, pick }: PreviousPickCardProps) {
               target="_blank"
               rel="noreferrer nofollow"
             >
-              <Button className="bg-gray-600 hover:bg-gray-700 gap-x-1 p-0 px-2">
+              <Button className="bg-gray-600 hover:bg-gray-700 gap-x-1 p-0 px-2 mr-2">
                 <SiMlb size={22} />
                 <p className="text-sm">Box</p>
               </Button>
